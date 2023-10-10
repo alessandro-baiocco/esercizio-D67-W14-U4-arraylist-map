5debdec844f3b61f9e7862fc6268abf3f6f32dae
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-10-10 15:54:03 +0200

    esercizio 2 parte 2 ok

--- src/Esercizio2.java
+++ src/Esercizio2.java
@@ -12,13 +12,25 @@ public class Esercizio2 {
             for (int i = 0; i < userInput; i++) {
                 number.add(rnd.nextInt(1, 100));
             }
+
+            number.sort(Integer::compareTo);
             System.out.println(number);
         } catch (InputMismatchException ex) {
             System.err.println("input non valido");
         } finally {
             input.close();
         }
+        esercizio2(number);
 
 
     }
+
+    public static void esercizio2(List<Integer> number) {
+        for (int i = number.size(); i > 0; i--) {
+            number.add(number.get(i - 1));
+        }
+        System.out.println(number);
+    }
+
+
 }
